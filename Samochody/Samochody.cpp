d3fe8ed8cd1b3e80943da232b14982c177eca9d1
IXOFIXOF <IXOF@POCZTA.ONET.PL>
2018-05-22 23:16:59 +0200

    Init commit

---
+++ Samochody/Samochody.cpp
@@ -0,0 +1,13 @@
+// Samochody.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+	std::cout << "Klasy bazowe samochody, ludzie i klasy pochodne samochody_osobowe, ciê¿arowe, van i maluch. Przeci¹¿yæ operator strumienia i dodawania i odejmowania ze stanu posiadania danego cz³owieka i parê wed³ug w³asnego pomys³u.\n";
+	std::cin.get();
+    return 0;
+}
+
